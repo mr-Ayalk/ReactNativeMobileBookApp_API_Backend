b00c3bd1f482bf44d69eec8461a982491be26963
mr-Ayalk <ayalkteketel7@gmail.com>
2025-08-11 12:23:21 +0300

    first commit

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const route = express.Router();
+
+route.post("/register", async (req, res) => {
+  res.send("register");
+});
+route.post("/login", async (req, res) => {
+  res.send("login");
+});
+
+export default route;
